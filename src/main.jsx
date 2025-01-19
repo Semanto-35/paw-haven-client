b59e445a458ad3830f5b3e4c4f0d2696ec72d4a1
Semanto-35 <sabbirhossainsemanto@gmail.com>
2025-01-19 11:16:43 +0600

    Initialize firebase configaration and authcontext

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
+import AuthProvider from './providers/AuthProvider.jsx'
 
 
 const queryClient = new QueryClient()
@@ -13,11 +14,13 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
-      </QueryClientProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <HelmetProvider>
+            <RouterProvider router={router} />
+          </HelmetProvider>
+        </QueryClientProvider>
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
