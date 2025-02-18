15d466c6848a35825cee3758decf4c78cbcb207b
Semanto-35 <sabbirhossainsemanto@gmail.com>
2025-02-18 23:37:38 +0600

    Implement Some adjustment and more responsiveness in the navbar

--- src/components/shared/DarkMode/DarkModeToggle.jsx
+++ src/components/shared/DarkMode/DarkModeToggle.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { Switch } from "@material-tailwind/react";
+import { IconButton } from "@material-tailwind/react";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
-
 
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = useState(localStorage.getItem("theme") === "dark");
@@ -17,16 +16,14 @@ const DarkModeToggle = () => {
   }, [darkMode]);
 
   return (
-    <div className="flex items-center space-x-2">
-      <SunIcon className="h-5 w-5 text-yellow-400 dark:text-gray-500" />
-      <Switch
-        checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
-        ripple={true}
-        color="blue"
-      />
-      <MoonIcon className="h-5 w-5 text-gray-900 dark:text-white" />
-    </div>
+    <IconButton
+      size="sm"
+      variant="text"
+      className="text-yellow-800 rounded-full dark:text-white"
+      onClick={() => setDarkMode(!darkMode)}
+    >
+      {darkMode ? <MoonIcon className="h-5 w-5" strokeWidth={2} /> : <SunIcon className="h-5 w-5" strokeWidth={2} />}
+    </IconButton>
   );
 };
 
--- src/components/shared/Navbar/NavbarComponent.jsx
+++ src/components/shared/Navbar/NavbarComponent.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { Navbar, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, ListItem, Typography, List, IconButton, Collapse, } from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon, IdentificationIcon } from "@heroicons/react/24/outline";
-import { HomeIcon, LifebuoyIcon, WalletIcon, PowerIcon } from "@heroicons/react/24/solid";
+import { Navbar, Button, Menu, MenuHandler, MenuList, MenuItem, Avatar, Typography, IconButton, Collapse, } from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon, } from "@heroicons/react/24/outline";
+import { HomeIcon, WalletIcon, PowerIcon, Squares2X2Icon, IdentificationIcon, } from "@heroicons/react/24/solid";
 import userImg from '../../../assets/icons/user.jpg';
 import useAuth from "../../../hooks/useAuth";
 import DarkModeToggle from "../DarkMode/DarkModeToggle";
+import Swal from "sweetalert2";
 
 
 
@@ -14,86 +15,111 @@ const NavbarComponent = () => {
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
-  const links = <>
-    <NavLink
-      to="/"
-      className={({ isActive }) => `font-medium ${isActive ? 'text-blue-500 underline' : 'text-blue-gray'}`}
-    >
-      <ListItem className="flex items-center gap-1 py-2 pr-4">
-        <HomeIcon className='w-5 h-5' />
-        <Typography variant="small" className="font-medium">
-          Home
-        </Typography>
-      </ListItem>
-    </NavLink>
-    <NavLink
-      to="/pets"
-      className={({ isActive }) => `font-medium ${isActive ? 'text-blue-500 underline' : 'text-blue-gray'}`}
-    >
-      <ListItem className="flex items-center gap-1 py-2 pr-4">
-        <LifebuoyIcon className='w-5 h-5' />
-        <Typography variant="small" className="font-medium">
-          Pet Listing
-        </Typography>
-      </ListItem>
-    </NavLink>
-    <NavLink
-      to="/donations"
-      className={({ isActive }) => `font-medium ${isActive ? 'text-blue-500 underline' : 'text-blue-gray'}`}
-    >
-      <ListItem className="flex items-center gap-1 py-2 pr-4">
-        <WalletIcon className='w-5 h-5' />
-        <Typography variant="small" className="font-medium">
-          Donation Campaigns
-        </Typography>
-      </ListItem>
-    </NavLink>
-  </>
+  useEffect(() => {
+    const handleResize = () => window.innerWidth >= 960 && setOpen(false);
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const navList = (
+    <ul className="my-2 flex flex-col gap-2 lg:my-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography as="li" variant="small" className="p-1 font-normal">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `flex items-center gap-2 ${isActive && "text-pink-500 font-semibold"} hover:text-pink-500`
+          }
+        >
+          <HomeIcon className="w-4 h-4 pb-[1px]" />
+          <span>Home</span>
+        </NavLink>
+      </Typography>
+
+      <Typography as="li" variant="small" className="p-1 font-normal">
+        <NavLink
+          to="/pets"
+          className={({ isActive }) =>
+            `flex items-center gap-2 ${isActive && "text-pink-500 font-semibold"} hover:text-pink-500`
+          }
+        >
+          <Squares2X2Icon className="w-4 h-4" />
+          <span>Pet Listing</span>
+        </NavLink>
+      </Typography>
+
+      <Typography as="li" variant="small" className="p-1 font-normal">
+        <NavLink
+          to="/donations"
+          className={({ isActive }) =>
+            `flex items-center gap-2 ${isActive && "text-pink-500 font-semibold"} hover:text-pink-500`
+          }
+        >
+          <WalletIcon className='w-4 h-4' />
+          <span>Donation Campaigns</span>
+        </NavLink>
+      </Typography>
+    </ul>
+  );
+
+  const handleLogout = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will be logged out!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, log out!",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        logOut();
+      }
+    });
+  };
+
 
   return (
-    <Navbar fullWidth className="mx-auto bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 max-w-7xl px-4 py-2 shadow-md">
-      <div className="flex items-center justify-between">
+    <Navbar fullWidth className="fixed top-0 left-0 z-50 bg-white dark:bg-black text-black dark:text-white border-none shadow-none p-0">
+      <div className="flex items-center justify-between  max-w-screen-2xl mx-auto py-3 px-4 shadow-sm">
         {/* Logo */}
-        <Link to="/" className="flex items-center gap-2">
-          <img className="w-12" src="https://i.ibb.co.com/69vhxMW/paw-1.png" alt="Logo" />
+        <Link to="/">
           <Typography
             variant="h4"
-            className=""
+            className="flex items-center gap-3"
           >
-            Pet Haven
+            <img className="w-12" src="https://i.ibb.co.com/69vhxMW/paw-1.png" alt="Logo" />
+            Paw Haven
           </Typography>
         </Link>
 
         {/* Navigation Links */}
         <div className="hidden lg:block">
-          <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-            {links}
-          </List>
+          {navList}
         </div>
 
-        {/* User Menu */}
-        <div className="flex justify-end gap-3">
-          <DarkModeToggle/>
+        <div className="flex items-center gap-3">
+          <DarkModeToggle />
+
+          {/* User Menu / auth buttons */}
           {user ? (
             <div className="hidden lg:block">
-              <Menu allowHover placement="bottom-end">
+              <Menu placement="bottom-end">
                 <MenuHandler>
                   <Avatar
                     variant="circular"
                     size="md"
                     alt="User"
                     withBorder
-                    color="blue-gray"
                     className="p-0.5"
                     src={user?.photoURL}
                   />
                 </MenuHandler>
-                <MenuList className="p-1">
+                <MenuList className="p-1 dark:bg-black bg-white text-black dark:text-white">
                   <div className="px-4 py-2">
-                    <Typography variant="h6" color="blue-gray">
+                    <Typography variant="h6">
                       {user?.displayName}
                     </Typography>
-                    <Typography variant="small" color="gray">
+                    <Typography variant="small">
                       {user?.email}
                     </Typography>
                   </div>
@@ -108,7 +134,7 @@ const NavbarComponent = () => {
                     </Typography>
                   </MenuItem>
                   <MenuItem
-                    onClick={logOut}
+                    onClick={handleLogout}
                     className="flex items-center gap-2 text-red-500"
                   >
                     <PowerIcon className="w-4 h-4" />
@@ -122,104 +148,103 @@ const NavbarComponent = () => {
           ) : (
             <div className="hidden gap-3 lg:flex">
               <Link to="/login">
-                <Button variant="text" size="sm" color="blue">
+                <Button variant="text" size="sm" color="pink">
                   LogIn
                 </Button>
               </Link>
               <Link to="/signUp">
-                <Button variant="gradient" color="blue-gray" size="sm">
+                <Button variant="filled" color="pink" size="sm">
                   Sign Up
                 </Button>
               </Link>
             </div>
           )}
-        </div>
 
-        {/* Mobile Menu Toggle */}
-        <IconButton
-          variant="text"
-          color="blue-gray"
-          className="lg:hidden"
-          onClick={() => setOpen(!open)}
-        >
-          {open ? <XMarkIcon className="h-6 w-6" strokeWidth={2} /> : <Bars3Icon className="h-6 w-6" strokeWidth={2} />}
-        </IconButton>
+          {/* Mobile Menu Toggle */}
+          <IconButton
+            variant="text"
+            className="lg:hidden text-black dark:text-white"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <XMarkIcon className="h-6 w-6" strokeWidth={2} /> : <Bars3Icon className="h-6 w-6" strokeWidth={2} />}
+          </IconButton>
+        </div>
       </div>
 
       {/* Mobile Navigation */}
-      <Collapse open={open} className="lg:hidden">
-        <List className="my-4 p-0">
-          {
-            user ? (
-              <div>
-                <div className="flex items-center gap-4">
-                  <Avatar src={user?.photoURL} alt="avatar" />
-                  <div>
-                    <Typography variant="h6">
-                      {user?.displayName}
-                    </Typography>
-                    <Typography variant="small" color="gray" className="font-normal">
-                      {user?.email}
-                    </Typography>
-                  </div>
-                </div>
-                <hr className='mt-3' />
-              </div>
-            ) : (
-              <div>
-                <div className="flex items-center gap-4">
-                  <Avatar src={userImg} alt="avatar" />
-                  <div>
-                    <Typography variant="h6">
-                      Username
-                    </Typography>
-                    <Typography variant="small" color="gray" className="font-normal">
-                      user@gmail.com
-                    </Typography>
-                  </div>
-                </div>
-                <hr className='mt-3' />
-              </div>
-            )
-          }
-          {links}
-          {
-            user ? (
-              <div className="w-full pb-4">
-                <ListItem className="flex items-center gap-1 py-2 pr-4" onClick={() => navigate('/dashboard')}
-                >
-                  <IdentificationIcon className='w-5 h-5' />
-                  <Typography variant="small" className="font-medium">
-                    Dashboard
+      <Collapse open={open} className="px-4 lg:hidden">
+        {
+          user ? (
+            <div>
+              <div className="flex items-center gap-4 mt-2">
+                <Avatar src={user?.photoURL} alt="avatar" />
+                <div>
+                  <Typography variant="h6">
+                    {user?.displayName}
                   </Typography>
-                </ListItem>
-                <Button
-                  onClick={logOut}
-                  className="mt-4 flex items-center gap-2"
-                  variant="text"
-                  size="md"
-                  color="red"
+                  <Typography variant="small" className="font-normal">
+                    {user?.email}
+                  </Typography>
+                </div>
+              </div>
+              <hr className='my-3' />
+            </div>
+          ) : (
+            <div>
+              <div className="flex items-center gap-4 mt-2">
+                <Avatar src={userImg} alt="avatar" />
+                <div>
+                  <Typography variant="h6">
+                    Username
+                  </Typography>
+                  <Typography variant="small" className="font-normal">
+                    user@gmail.com
+                  </Typography>
+                </div>
+              </div>
+              <hr className='my-3' />
+            </div>
+          )
+        }
+        {navList}
+        {
+          user ? (
+            <div className="w-full pb-4">
+              <Typography as="li" variant="small" className="p-1">
+                <Link
+                  to="/dashboard"
+                  className="flex items-center gap-2 hover:text-pink-500"
                 >
-                  <PowerIcon className="w-4 h-4" />
-                  Log Out
+                  <IdentificationIcon className='w-4 h-4' />
+                  <span>Dashboard</span>
+                </Link>
+              </Typography>
+              <Button
+                onClick={handleLogout}
+                className="mt-4 flex items-center gap-2"
+                variant="text"
+                size="md"
+                color="red"
+              >
+                <PowerIcon className="w-4 h-4" />
+                Log Out
+              </Button>
+            </div>
+          ) : (
+            <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden py-4">
+              <Link to="/login" className="w-1/2">
+                <Button variant="outlined" size="sm" color="pink" fullWidth>
+                  LogIn
                 </Button>
-              </div>
-            ) : (
-              <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden py-4">
-                <Link to="/login" className="w-1/2">
-                  <Button variant="outlined" size="sm" color="blue" fullWidth>
-                    LogIn
-                  </Button>
-                </Link>
-                <Link to="/signUp" className="w-1/2">
-                  <Button variant="gradient" size="sm" color="gray" fullWidth>
-                    Sign Up
-                  </Button>
-                </Link>
-              </div>
-            )
-          }
-        </List>
+              </Link>
+              <Link to="/signUp" className="w-1/2">
+                <Button variant="filled" size="sm" color="pink" fullWidth>
+                  Sign Up
+                </Button>
+              </Link>
+            </div>
+          )
+        }
       </Collapse>
     </Navbar>
   );
